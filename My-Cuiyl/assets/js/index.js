1ddd5655e53b3e6ff13a17cec53f42b323e82049
hsl947 <1506070803@qq.com>
2017-03-24 15:38:21 +0800

    My-Cuiyl

--- My-Cuiyl/assets/js/index.js
+++ My-Cuiyl/assets/js/index.js
@@ -2,33 +2,33 @@
 var init = function init() {
     //瀑布流图片代码
     var img_src = [{
-        src: "assets/images/1.jpg"
+        src: "assets/images/bear-avatar.png"
     }, {
-            src: "assets/images/2.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/3.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/4.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/5.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/6.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/7.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/8.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/9.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/10.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/11.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/12.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/13.jpg"
+            src: "assets/images/bear-avatar.png"
         }, {
-            src: "assets/images/14.jpg"
+            src: "assets/images/bear-avatar.png"
         }];
     var i = 0,
         sum;
