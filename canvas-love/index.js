f311cf50a92d1f8e6bb17c2f16c9eef7ac91d24a
hsl947 <1506070803@qq.com>
2017-03-31 15:42:56 +0800

    commit

--- canvas-love/index.js
+++ canvas-love/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('爱|你|哦~|翠|姐|#rectangle|#countdown 3|Go~||');
+      S.UI.simulate('#countdown 3|爱|你|哦~|翠|姐|#rectangle|Go~||');
     }
 
     S.Drawing.loop(function () {
