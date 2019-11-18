16c6f21ad5c17aaeb8e275f5ca83549b3f20287a
hsl947 <1506070803@qq.com>
2019-11-18 13:33:41 +0800

    test

---
+++ react-test/precache-manifest.08ee72d57ace43791649d8e8dd70c48c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4463d28e8043b5276177d8a9f4f83e95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b061e31f2fc84ee2e5a0",
+    "url": "/static/css/2.b3846d1d.chunk.css"
+  },
+  {
+    "revision": "0981092e56a283cf43ef",
+    "url": "/static/css/main.a0880ab6.chunk.css"
+  },
+  {
+    "revision": "b061e31f2fc84ee2e5a0",
+    "url": "/static/js/2.8dba6367.chunk.js"
+  },
+  {
+    "revision": "0981092e56a283cf43ef",
+    "url": "/static/js/main.bdedb64f.chunk.js"
+  },
+  {
+    "revision": "d653c3e1ff0c4fc20192",
+    "url": "/static/js/runtime-main.d01d61ca.js"
+  }
+]);
